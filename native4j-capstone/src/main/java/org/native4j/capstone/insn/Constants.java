9f9b89e826cc370949f2e3f53bcebc4f3273f454
jus7n <126373733+jus7n@users.noreply.github.com>
2023-08-12 20:53:35 -0600

    add missing constants

---
+++ native4j-capstone/src/main/java/org/native4j/capstone/insn/Constants.java
@@ -0,0 +1,16 @@
+package org.native4j.capstone.insn;
+
+/**
+ * Generic Capstone constants.
+ */
+@SuppressWarnings("ALL")
+public class Constants {
+    public static final int CS_GRP_INVALID = 0;
+    public static final int CS_GRP_JUMP = 1;
+    public static final int CS_GRP_CALL = 2;
+    public static final int CS_GRP_RET = 3;
+    public static final int CS_GRP_INT = 4;
+    public static final int CS_GRP_IRET = 5;
+    public static final int CS_GRP_PRIVILEGE = 6;
+    public static final int CS_GRP_BRANCH_RELATIVE = 7;
+}

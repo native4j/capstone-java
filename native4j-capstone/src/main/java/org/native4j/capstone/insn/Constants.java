a124d17fc845ce977620e487d03f90a3babcec60
jus7n <126373733+jus7n@users.noreply.github.com>
2023-08-12 21:00:38 -0600

    fix spotless violations

--- native4j-capstone/src/main/java/org/native4j/capstone/insn/Constants.java
+++ native4j-capstone/src/main/java/org/native4j/capstone/insn/Constants.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright 2023 The Native4J Authors
+ *
+ * Use of this source code is governed by the MIT license found in the LICENSE
+ * file.
+ */
 package org.native4j.capstone.insn;
 
 /**
